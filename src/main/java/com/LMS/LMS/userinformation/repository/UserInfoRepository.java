3ff3823a72dce40daf2f7f7b346ea831d661e3bb
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2025-12-19 12:05:33 +0545

    feat|adding UserInfoRepository class

---
+++ src/main/java/com/LMS/LMS/userinformation/repository/UserInfoRepository.java
@@ -0,0 +1,12 @@
+package com.LMS.LMS.userinformation.repository;
+
+import com.LMS.LMS.userinformation.model.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo,Long> {
+
+    public Optional<UserInfo> findByUserName(String username);
+}
