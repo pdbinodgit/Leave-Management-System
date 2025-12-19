cf9ebc3ed0aa5d3917d5f6d967c8d55c47a4f0ae
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2025-12-19 21:15:40 +0545

    feat| working on user service information

--- src/main/java/com/LMS/LMS/userinformation/service/UserInfoService.java
+++ src/main/java/com/LMS/LMS/userinformation/service/UserInfoService.java
@@ -1,5 +1,14 @@
 package com.LMS.LMS.userinformation.service;
 
+import com.LMS.LMS.userinformation.dto.UserInfoDto;
+import com.LMS.LMS.userinformation.model.UserInfo;
+
+import java.util.List;
+
 public interface UserInfoService {
+
+    public UserInfoDto saveUserInfo(UserInfoDto dto);
+
+    public List<UserInfoDto> getAllUser();
 
 }
