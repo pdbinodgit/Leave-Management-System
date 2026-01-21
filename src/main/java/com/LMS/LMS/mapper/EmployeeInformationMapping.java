f49c62bbb224a78f438df5f0447fa8155df2ff28
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2026-01-21 21:22:04 +0545

    feat| working on employee mapper

--- src/main/java/com/LMS/LMS/mapper/EmployeeInformationMapping.java
+++ src/main/java/com/LMS/LMS/mapper/EmployeeInformationMapping.java
@@ -1,4 +1,11 @@
 package com.LMS.LMS.mapper;
 
+import com.LMS.LMS.employee.dto.EmployeeInformationDto;
+import com.LMS.LMS.employee.model.EmployeeInformation;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface EmployeeInformationMapping {
+    public EmployeeInformationDto entityToDto(EmployeeInformation empInfo);
+    public EmployeeInformation dtoToEntity(EmployeeInformationDto dto);
 }
