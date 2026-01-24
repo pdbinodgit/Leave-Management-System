79c1b405d79d612b8edd86683b507bf720781e32
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2026-01-24 20:19:52 +0545

    feat|Leave :working on leave repository

---
+++ src/main/java/com/LMS/LMS/leave/repository/LeaveRepo.java
@@ -0,0 +1,12 @@
+package com.LMS.LMS.leave.repository;
+
+import com.LMS.LMS.leave.model.LeaveInformation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface LeaveRepo extends JpaRepository<LeaveInformation,Long> {
+
+    public Optional<LeaveInformation> findByPresentStatus(boolean status);
+}
