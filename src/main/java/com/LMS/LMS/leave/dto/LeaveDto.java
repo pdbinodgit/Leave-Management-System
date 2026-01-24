7dfbc8424809f95a4a53b27f085156b6640d7e01
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2026-01-24 20:07:43 +0545

    feat|Leave :working on leave class

--- src/main/java/com/LMS/LMS/leave/dto/LeaveDto.java
+++ src/main/java/com/LMS/LMS/leave/dto/LeaveDto.java
@@ -1,13 +1,7 @@
 package com.LMS.LMS.leave.dto;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -17,12 +11,8 @@ public class LeaveDto {
 
     private Long id;
     private String leaveType;
-
     private Long totalLeave;
-
     private Boolean presentStatus;
-
     private LocalDateTime createdAt;
-
     private LocalDateTime updatedAt;
 }
