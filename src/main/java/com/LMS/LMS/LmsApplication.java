3feb9b594857980010c31faaee062c02c7ba98f9
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2026-05-29 20:50:09 +0545

    cors config

--- src/main/java/com/LMS/LMS/LmsApplication.java
+++ src/main/java/com/LMS/LMS/LmsApplication.java
@@ -1,16 +1,16 @@
 package com.LMS.LMS;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+;
 
 @SpringBootApplication
 public class LmsApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(LmsApplication.class, args);
 	}
-
-
-
 
 }
--- src/main/java/com/LMS/LMS/auth/jwt/SecurityConfig.java
+++ src/main/java/com/LMS/LMS/auth/jwt/SecurityConfig.java
@@ -12,6 +12,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -31,6 +36,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth->auth
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
 
@@ -45,5 +51,19 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
 
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of("http://127.0.0.1:5500")); // your frontend
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
 }
